6072daf580e0e299d9c5e95291f3bea379f41779
Marcus Spangenberg <marcus.spangenberg@symphony.com>
2021-12-01 12:09:10 +0100

    -

--- Jenkins/PRUnitTestRunnerRelease.groovy
+++ Jenkins/PRUnitTestRunnerRelease.groovy
@@ -1,51 +1,53 @@
 @Library('SFE-RTC-pipeline@marcus/sha_check') _
 
 void prRunner(String cmakeBuildType) {
-    checkout scm
+    stage("Checkout") {
+        checkout scm
+    }
 
-    docker.image('gcr.io/sym-dev-rtc/buildsmb-ubuntu-focal:latest').inside {
-        env.GIT_COMMITTER_NAME = "Jenkins deployment job"
-        env.GIT_COMMITTER_EMAIL = "jenkinsauto@symphony.com"
-        sh "docker/ubuntu-focal/buildscript.sh $cmakeBuildType"
-        sh "docker/ubuntu-focal/runtests.sh"
+    stage("Build and test") {
+        docker.image('gcr.io/sym-dev-rtc/buildsmb-ubuntu-focal:latest').inside {
+            env.GIT_COMMITTER_NAME = "Jenkins deployment job"
+            env.GIT_COMMITTER_EMAIL = "jenkinsauto@symphony.com"
+            sh "docker/ubuntu-focal/buildscript.sh $cmakeBuildType"
+            sh "docker/ubuntu-focal/runtests.sh"
+        }
     }
 }
 
 abortPreviousRunningBuilds()
 
-parallel {
-    stage("Release") {
-        node('be-integration') {
-            prRunner("Release")
-        }
+parallel "Release": {
+    node('be-integration') {
+        prRunner("Release")
     }
-    stage("LCheck") {
-        node('be-integration') {
-            prRunner("LCheck")
-        }
+},
+parallel "LCheck": {
+    node('be-integration') {
+        prRunner("LCheck")
     }
-    stage("TCheck") {
-        node('be-integration') {
-            prRunner("TCheck")
-        }
+},
+parallel "TCheck": {
+    node('be-integration') {
+        prRunner("TCheck")
     }
-    stage("DCheck") {
-        node('be-integration') {
-            try {
-                prRunner("DCheck")
-            } finally {
-                stage("Post Actions") {
-                    dir ("ubuntu-focal/smb") {
-                        junit testResults: "test-results.xml"
-                        publishHTML(target: [
-                                allowMissing         : false,
-                                alwaysLinkToLastBuild: false,
-                                keepAll              : true,
-                                reportDir            : "coverage",
-                                reportFiles          : "index.html",
-                                reportName           : "Code Coverage Report"
-                        ])
-                    }
+},
+parallel "DCheck": {
+    node('be-integration') {
+        try {
+            prRunner("DCheck")
+        } finally {
+            stage("Post Actions") {
+                dir ("ubuntu-focal/smb") {
+                    junit testResults: "test-results.xml"
+                    publishHTML(target: [
+                            allowMissing         : false,
+                            alwaysLinkToLastBuild: false,
+                            keepAll              : true,
+                            reportDir            : "coverage",
+                            reportFiles          : "index.html",
+                            reportName           : "Code Coverage Report"
+                    ])
                 }
             }
         }
