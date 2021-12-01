c3127c1f592062cb711f135a52eb79b8c642779d
Marcus Spangenberg <marcus.spangenberg@symphony.com>
2021-12-01 12:03:03 +0100

    -

--- Jenkins/PRUnitTestRunnerRelease.groovy
+++ Jenkins/PRUnitTestRunnerRelease.groovy
@@ -1,43 +1,36 @@
 @Library('SFE-RTC-pipeline@marcus/sha_check') _
 
 void prRunner(String cmakeBuildType) {
-    stage('Checkout') {
-        checkout scm
-    }
+    checkout scm
 
     docker.image('gcr.io/sym-dev-rtc/buildsmb-ubuntu-focal:latest').inside {
-        stage("Build ubuntu-focal [$cmakeBuildType]") {
-            env.GIT_COMMITTER_NAME = "Jenkins deployment job"
-            env.GIT_COMMITTER_EMAIL = "jenkinsauto@symphony.com"
-            sh "docker/ubuntu-focal/buildscript.sh $cmakeBuildType"
-        }
-
-        stage('Run tests ubuntu-focal') {
-            sh "docker/ubuntu-focal/runtests.sh"
-        }
+        env.GIT_COMMITTER_NAME = "Jenkins deployment job"
+        env.GIT_COMMITTER_EMAIL = "jenkinsauto@symphony.com"
+        sh "docker/ubuntu-focal/buildscript.sh $cmakeBuildType"
+        sh "docker/ubuntu-focal/runtests.sh"
     }
 }
 
 abortPreviousRunningBuilds()
 
-parallel {
-    steps {
-        release: {
+stages {
+    parallel {
+        stage("Release") {
             node('be-integration') {
                 prRunner("Release")
             }
-        },
-        lcheck: {
+        }
+        stage("LCheck") {
             node('be-integration') {
                 prRunner("LCheck")
             }
-        },
-        tcheck: {
+        }
+        stage("TCheck") {
             node('be-integration') {
                 prRunner("TCheck")
             }
-        },
-        dcheck: {
+        }
+        stage("DCheck") {
             node('be-integration') {
                 try {
                     prRunner("DCheck")
