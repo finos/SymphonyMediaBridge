60bc1fb4c1568d4532a1e40c15279e7052f32ad7
Marcus Spangenberg <marcus.spangenberg@symphony.com>
2021-12-01 09:29:29 +0100

    Single PR builder

--- Jenkins/PRUnitTestRunnerRelease.groovy
+++ Jenkins/PRUnitTestRunnerRelease.groovy
@@ -7,31 +7,9 @@ void prRunner(String cmakeBuildType) {
             env.GIT_COMMITTER_EMAIL = "jenkinsauto@symphony.com"
             sh "docker/ubuntu-focal/buildscript.sh $cmakeBuildType"
         }
-    }
 
-    try {
-        docker.image('gcr.io/sym-dev-rtc/buildsmb-ubuntu-focal:latest').inside {
-            stage('Run tests centos7') {
-                sh "docker/el7/runtests.sh"
-            }
-        }
-    } finally {
-        stage("Post Actions") {
-            dir ("ubuntu-focal/smb") {
-                junit testResults: "test-results.xml"
-
-                hasCoverage = (cmakeBuildType == "DCheck");
-                if (hasCoverage) {
-                    publishHTML(target: [
-                            allowMissing         : false,
-                            alwaysLinkToLastBuild: false,
-                            keepAll              : true,
-                            reportDir            : "coverage",
-                            reportFiles          : "index.html",
-                            reportName           : "Code Coverage Report"
-                    ])
-                }
-            }
+        stage('Run tests ubuntu-focal') {
+            sh "docker/ubuntu-focal/runtests.sh"
         }
     }
 }
@@ -43,19 +21,25 @@ node('be-integration') {
         checkout scm
     }
 
-    stage('Release') {
-        prRunner("Release")
-    }
+    prRunner("Release")
+    prRunner("LCheck")
+    prRunner("TCheck")
 
-    stage('DCheck') {
+    try {
         prRunner("DCheck")
-    }
-
-    stage('LCheck') {
-        prRunner("LCheck")
-    }
-
-    stage('TCheck') {
-        prRunner("TCheck")
+    } finally {
+        stage("Post Actions") {
+            dir ("ubuntu-focal/smb") {
+                junit testResults: "test-results.xml"
+                publishHTML(target: [
+                        allowMissing         : false,
+                        alwaysLinkToLastBuild: false,
+                        keepAll              : true,
+                        reportDir            : "coverage",
+                        reportFiles          : "index.html",
+                        reportName           : "Code Coverage Report"
+                ])
+            }
+        }
     }
 }
