a62ed114ce8c30165ba9694e516b6b88fd377182
Marcus Spangenberg <marcus.spangenberg@symphony.com>
2021-12-01 09:11:34 +0100

    Single PR builder

--- Jenkins/PRUnitTestRunnerRelease.groovy
+++ Jenkins/PRUnitTestRunnerRelease.groovy
@@ -1,15 +1,61 @@
 @Library('SFE-RTC-pipeline@marcus/sha_check') _
 
-cmakeBuildType = "Release"
+void prRunner(String cmakeBuildType) {
+    docker.image('gcr.io/sym-dev-rtc/buildsmb-ubuntu-focal:latest').inside {
+        stage("Build ubuntu-focal [$cmakeBuildType]") {
+            env.GIT_COMMITTER_NAME = "Jenkins deployment job"
+            env.GIT_COMMITTER_EMAIL = "jenkinsauto@symphony.com"
+            sh "docker/ubuntu-focal/buildscript.sh $cmakeBuildType"
+        }
+    }
+
+    try {
+        docker.image('gcr.io/sym-dev-rtc/buildsmb-ubuntu-focal:latest').inside {
+            stage('Run tests centos7') {
+                sh "docker/el7/runtests.sh"
+            }
+        }
+    } finally {
+        stage("Post Actions") {
+            dir ("ubuntu-focal/smb") {
+                junit testResults: "test-results.xml"
+
+                hasCoverage = (cmakeBuildType == "DCheck");
+                if (hasCoverage) {
+                    publishHTML(target: [
+                            allowMissing         : false,
+                            alwaysLinkToLastBuild: false,
+                            keepAll              : true,
+                            reportDir            : "coverage",
+                            reportFiles          : "index.html",
+                            reportName           : "Code Coverage Report"
+                    ])
+                }
+            }
+        }
+    }
+}
 
 abortPreviousRunningBuilds()
 
-notifyPRStatus("https://github.com/finos/SymphonyMediaBridge", "Build and Unit Testing of ${env.BRANCH_NAME} [$cmakeBuildType]") {
-    node('be-integration') {
-        stage('Checkout') {
-            checkout scm
-        }
-        prRunner = load('Jenkins/PRUnitTestRunner.groovy')
-        prRunner.run(cmakeBuildType)
+node('be-integration') {
+    stage('Checkout') {
+        checkout scm
+    }
+
+    stage('Release') {
+        prRunner("Release")
+    }
+
+    stage('DCheck') {
+        prRunner("DCheck")
+    }
+
+    stage('LCheck') {
+        prRunner("LCheck")
+    }
+
+    stage('TCheck') {
+        prRunner("TCheck")
     }
 }
