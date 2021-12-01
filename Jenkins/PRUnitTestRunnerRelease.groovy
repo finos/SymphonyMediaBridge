f196ef206a1d24324a3d53bc83442dda5fb4cf7e
Marcus Spangenberg <marcus.spangenberg@symphony.com>
2021-12-01 11:57:52 +0100

    Testing parallel pipeline

--- Jenkins/PRUnitTestRunnerRelease.groovy
+++ Jenkins/PRUnitTestRunnerRelease.groovy
@@ -1,6 +1,10 @@
 @Library('SFE-RTC-pipeline@marcus/sha_check') _
 
 void prRunner(String cmakeBuildType) {
+    stage('Checkout') {
+        checkout scm
+    }
+
     docker.image('gcr.io/sym-dev-rtc/buildsmb-ubuntu-focal:latest').inside {
         stage("Build ubuntu-focal [$cmakeBuildType]") {
             env.GIT_COMMITTER_NAME = "Jenkins deployment job"
@@ -16,29 +20,41 @@ void prRunner(String cmakeBuildType) {
 
 abortPreviousRunningBuilds()
 
-node('be-integration') {
-    stage('Checkout') {
-        checkout scm
-    }
-
-    prRunner("Release")
-    prRunner("LCheck")
-    prRunner("TCheck")
-
-    try {
-        prRunner("DCheck")
-    } finally {
-        stage("Post Actions") {
-            dir ("ubuntu-focal/smb") {
-                junit testResults: "test-results.xml"
-                publishHTML(target: [
-                        allowMissing         : false,
-                        alwaysLinkToLastBuild: false,
-                        keepAll              : true,
-                        reportDir            : "coverage",
-                        reportFiles          : "index.html",
-                        reportName           : "Code Coverage Report"
-                ])
+parallel {
+    steps {
+        release: {
+            node('be-integration') {
+                prRunner("Release")
+        },
+        lcheck: {
+            node('be-integration') {
+                prRunner("LCheck")
+            }
+        },
+        tcheck: {
+            node('be-integration') {
+                prRunner("TCheck")
+            }
+        },
+        dcheck: {
+            node('be-integration') {
+                try {
+                    prRunner("DCheck")
+                } finally {
+                    stage("Post Actions") {
+                        dir ("ubuntu-focal/smb") {
+                            junit testResults: "test-results.xml"
+                            publishHTML(target: [
+                                    allowMissing         : false,
+                                    alwaysLinkToLastBuild: false,
+                                    keepAll              : true,
+                                    reportDir            : "coverage",
+                                    reportFiles          : "index.html",
+                                    reportName           : "Code Coverage Report"
+                            ])
+                        }
+                    }
+                }
             }
         }
     }
