0fa50c8293d77235b3426ba08dda378243a961c2
Olof Kallander <olof.kallander@symphony.com>
2022-06-01 22:28:15 +0200

    -

--- memory/PartialSortExtractor.h
+++ memory/PartialSortExtractor.h
@@ -6,7 +6,11 @@ template <typename T>
 class PartialSortExtractor
 {
 public:
-    PartialSortExtractor(T* startIt, T* endIt) : _heap(startIt), _heapEnd(endIt) { std::make_heap(startIt, endIt); }
+    PartialSortExtractor(T* startIt, T* endIt) : _heap(startIt), _heapEnd(endIt)
+    {
+        static_assert(std::is_trivial<T>(), "T must be a trivial type");
+        std::make_heap(startIt, endIt);
+    }
 
     const T& top() const { return *_heap; }
     T& top() { return *_heap; }
