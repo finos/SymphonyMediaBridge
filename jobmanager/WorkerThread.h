1bac6908f65141cf82dd5d4a850b93d54b197133
Ricardo.Domingues <ricardo.domingues@symphony.com>
2022-08-31 17:45:14 +0100

    Add comments

--- jobmanager/WorkerThread.h
+++ jobmanager/WorkerThread.h
@@ -16,7 +16,17 @@ public:
     static double getWaitTime(); // ms
     static double getWorkTime(); // ms
 
+    /**
+     * @brief Checks whether current thread is a WorkerThread or not
+     * @return true if current thread is a worker thread; otherwise, false.
+     */
     static bool isWorkerThread();
+
+    /**
+     * @brief Causes the calling thread to yield execution to another job that is ready to run on
+     * jobManager attached to current WorkerThread.
+     * @return true if has executed to another job; otherwise, false.
+     */
     static bool yield();
 
 private:
