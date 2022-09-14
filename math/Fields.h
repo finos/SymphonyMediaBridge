0a79d053abcb914b64295cedb596c319a17b1653
Olof Kallander <olof.kallander@symphony.com>
2022-09-14 23:09:26 +0200

    -

--- math/Fields.h
+++ math/Fields.h
@@ -11,10 +11,10 @@ constexpr typename std::make_signed<T>::type ringDifference(T a, const T b)
 {
     using SignedT = typename std::make_signed<T>::type;
     static_assert(std::is_unsigned<T>::value, "Difference only works on unsigned integers");
-    assert(sizeof(T) * 8 == BITS || a < (T(1) << BITS));
-    assert(sizeof(T) * 8 == BITS || b < (T(1) << BITS));
 
     constexpr T mask = (T(0) - 1) >> (sizeof(T) * 8 - BITS);
+    assert(a <= mask);
+    assert(b <= mask);
 
     T v = (b - a) & mask;
 
