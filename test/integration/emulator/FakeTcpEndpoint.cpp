c1389b908e2d0b9fe905db7d70db1427087e40c4
Olof Kallander <olof.kallander@symphony.com>
2023-04-25 13:16:50 +0200

    -

--- test/integration/emulator/FakeTcpEndpoint.cpp
+++ test/integration/emulator/FakeTcpEndpoint.cpp
@@ -7,7 +7,7 @@
 namespace emulator
 {
 uint16_t FakeTcpEndpoint::_portCounter = 1000;
-int FakeTcpEndpoint::_fdGenerator = 1;
+std::atomic_int FakeTcpEndpoint::_fdGenerator(1);
 
 FakeTcpEndpoint::FakeTcpEndpoint(jobmanager::JobManager& jobManager,
     memory::PacketPoolAllocator& allocator,
