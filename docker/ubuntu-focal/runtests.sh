0cbf2ec48525e02ff4e042e5a685f5eff82d7ddf
Olof Kallander <olof.kallander@symphony.com>
2023-05-26 17:20:26 +0200

    test results

--- docker/ubuntu-focal/runtests.sh
+++ docker/ubuntu-focal/runtests.sh
@@ -28,6 +28,13 @@ export LD_LIBRARY_PATH=../../ubuntu-focal/smb/libs
 if [ $? != 0 ]; then
     pr "Testing failed"; exit 1
 fi
+
+pr "Running tests2"
+../../ubuntu-focal-deb/smb/UnitTest2 --gtest_filter="*.*" --gtest_output=xml:../../ubuntu-focal-deb/smb/test-results2.xml
+if [ $? != 0 ]; then
+    pr "Testing failed"; exit 1
+fi
+
 popd || exit 1
 
 pushd ubuntu-focal/smb || exit 1
