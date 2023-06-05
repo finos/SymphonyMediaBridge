a95d7f4878a650d2a204d2ee252102c435742164
Daniel Gronberg <daniel.gronberg@symphony.com>
2023-06-05 16:02:41 +0200

    Remove configure docker stage in jenkins files

--- Jenkins/PRUnitTestRunner.groovy
+++ Jenkins/PRUnitTestRunner.groovy
@@ -5,10 +5,6 @@ void prRunner(String cmakeBuildType, String platform, String dockerTag) {
         checkout scm
     }
 
-    stage('Configure Docker') {
-        sh "gcloud auth configure-docker gcr.io --quiet"
-    }
-
     stage("Build\n[$cmakeBuildType $platform]") {
         docker.image("gcr.io/sym-dev-rtc/buildsmb-$platform:$dockerTag").inside {
             env.GIT_COMMITTER_NAME = "Jenkins deployment job"
@@ -32,33 +28,33 @@ void prRunner(String cmakeBuildType, String platform, String dockerTag) {
 }
 
 abortPreviousRunningBuilds()
-
+// TODO: "Change back to be-integration before merge"
 parallel "Release el7": {
-    node('be-integration') {
+    node('rtc-14160-test')
         prRunner("Release", "el7", "1f7ef85")
     }
 }, "Release AWS-linux": {
-    node('be-integration') {
+    node('rtc-14160-test')
         prRunner("Release", "aws-linux", "latest")
     }
 }, "Release el8": {
-    node('be-integration') {
+    node('rtc-14160-test')
         prRunner("Release", "el8", "latest")
     }
 }, "LCheck": {
-    node('be-integration') {
+    node('rtc-14160-test')
         prRunner("LCheck", "el8", "latest")
     }
 }, "TCheck": {
-    node('be-integration') {
+    node('rtc-14160-test')
         prRunner("TCheck", "el8", "latest")
     }
 }, "DCheck": {
-    node('be-integration') {
+    node('rtc-14160-test')
         prRunner("DCheck", "el8", "latest")
     }
 }, "LCov": {
-    node('be-integration') {
+    node('rtc-14160-test')
         try {
             prRunner("LCov", "el8", "latest")
         } finally {
@@ -78,7 +74,7 @@ parallel "Release el7": {
         }
     }
 }, "Release Ubuntu public": {
-    node('be-integration') {
+    node('rtc-14160-test')
         prRunner("Release", "ubuntu-focal-deb", "latest")
     }
 }
