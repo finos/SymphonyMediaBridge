76d574f7096f77c39d1b60737369e73069b6d21d
Olof Källander <whistler.boardrider@gmail.com>
2023-11-04 13:53:31 +0100

    cholesky update

--- math/Matrix.h
+++ math/Matrix.h
@@ -12,6 +12,8 @@ namespace math
 template <typename T, uint32_t M, uint32_t N = 1>
 class Matrix
 {
+    static_assert(M > 0 && N > 0);
+
 public:
     Matrix()
     {
@@ -52,7 +54,22 @@ public:
         return *this;
     }
 
-    bool operator==(const Matrix<T, M, N>& other) const { return 0 == std::memcmp(&_m, &other._m, sizeof(_m)); }
+    bool operator==(const Matrix<T, M, N>& other) const
+    {
+        for (uint32_t i = 0; i < M; ++i)
+        {
+            for (uint32_t j = 0; j < N; ++j)
+            {
+                if (_m[i][j] != other._m[i][j])
+                {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    bool operator!=(const Matrix<T, M, N>& other) const { return !(*this == other); }
 
     Matrix& operator+=(const Matrix<T, M, N>& m2)
     {
@@ -76,6 +93,19 @@ public:
             }
         }
         return *this;
+    }
+
+    Matrix operator-() const
+    {
+        Matrix<T, M, N> n;
+        for (uint32_t i = 0; i < M; ++i)
+        {
+            for (uint32_t j = 0; j < N; ++j)
+            {
+                n(i, j) = -_m[i][j];
+            }
+        }
+        return n;
     }
 
     inline T& operator()(uint32_t r, uint32_t c = 0)
@@ -222,24 +252,28 @@ Matrix<T, M, M> choleskyDecompositionLL(const Matrix<T, M, M>& m)
             {
                 for (uint32_t k = 0; k < j; k++)
                 {
-                    sum += (lowerTriangular(i, k) * lowerTriangular(i, k));
+                    sum += lowerTriangular(i, k) * lowerTriangular(i, k);
                 }
 
-                auto d = m(i, i) - sum;
+                const auto d = m(i, i) - sum;
                 lowerTriangular(j, j) = std::sqrt(std::abs(d));
             }
             else
             {
                 for (uint32_t k = 0; k < j; k++)
                 {
-                    sum += (lowerTriangular(i, k) * lowerTriangular(j, k));
+                    sum += lowerTriangular(i, k) * lowerTriangular(j, k);
                 }
-                auto d = m(i, j) - sum;
+                const auto d = m(i, j) - sum;
                 if (lowerTriangular(j, j) == 0)
                 {
                     assert(std::abs(d) < 1E-20);
+                    lowerTriangular(i, j) = 0;
                 }
-                lowerTriangular(i, j) = (lowerTriangular(j, j) != 0 ? d / lowerTriangular(j, j) : 0);
+                else
+                {
+                    lowerTriangular(i, j) = d / lowerTriangular(j, j);
+                }
             }
         }
     }
@@ -282,4 +316,405 @@ bool isValid(const Matrix<T, M, N>& m)
     return true;
 }
 
+// Frobenius norm ov matrix, and "norm" of vector
+template <typename T, uint32_t M, uint32_t N>
+T norm(const Matrix<T, M, N>& m)
+{
+    T n = 0;
+    for (auto i = 0u; i < M; ++i)
+    {
+        for (auto j = 0u; j < N; ++j)
+        {
+            n += m(i, j) * m(i, j);
+        }
+    }
+    return std::sqrt(n);
+}
+
+template <typename T, uint32_t M, uint32_t N>
+T min(const Matrix<T, M, N>& m)
+{
+    T n = m(0, 0);
+    for (auto i = 0u; i < M; ++i)
+    {
+        for (auto j = 0u; j < N; ++j)
+        {
+            n = std::min(n, m(i, j));
+        }
+    }
+    return n;
+}
+
+template <typename T, uint32_t M, uint32_t N>
+T max(const Matrix<T, M, N>& m)
+{
+    T n = m(0, 0);
+    for (auto i = 0u; i < M; ++i)
+    {
+        for (auto j = 0u; j < N; ++j)
+        {
+            n = std::max(n, m(i, j));
+        }
+    }
+    return n;
+}
+
+template <typename T, uint32_t M>
+bool isSymmetric(const Matrix<T, M, M>& m)
+{
+    for (auto i = 1u; i < M; ++i)
+    {
+        for (auto j = 0u; j < i; ++j)
+        {
+            if (m(i, j) != m(j, i))
+            {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+template <typename T, uint32_t M>
+void makeSymmetric(Matrix<T, M, M>& m)
+{
+    for (auto i = 1u; i < M; ++i)
+    {
+        for (auto j = 0u; j < i; ++j)
+        {
+            const auto v = (m(i, j) + m(j, i)) / 2.0;
+            m(i, j) = v;
+            m(j, i) = v;
+        }
+    }
+}
+
+// swaps selected row with a lower row to make sure smallest absolute value is at row cr column cr.
+// Multiplies the swapped row with -1 to preserve determinant
+template <typename T, uint32_t M, uint32_t N>
+void pivotRows(Matrix<T, M, N>& m, uint32_t cr)
+{
+    static_assert(std::is_signed<T>::value);
+    static_assert(N >= M);
+    T candidateValue = std::abs(m(cr, cr));
+    auto candidatePos = cr;
+    for (auto i = cr + 1; i < M; ++i)
+    {
+        auto v = std::abs(m(i, cr));
+        if (v > 0 && (v < candidateValue || candidateValue == 0))
+        {
+            candidatePos = i;
+            candidateValue = v;
+        }
+    }
+
+    if (candidatePos == cr)
+    {
+        return;
+    }
+
+    bool signIsDifferent = std::signbit(m(cr, cr)) != std::signbit(m(candidatePos, cr));
+    for (auto j = 0u; j < N; ++j)
+    {
+        std::swap(m(cr, j), m(candidatePos, j));
+        if (signIsDifferent)
+        {
+            m(cr, j) *= -1;
+        }
+        else
+        {
+            m(candidatePos, j) *= -1;
+        }
+    }
+
+    return;
+}
+
+template <typename T, uint32_t M, uint32_t N>
+bool gaussianElimination(Matrix<T, M, N>& original)
+{
+    Matrix<T, M, N> m = original;
+    // along diagonal
+    for (auto i = 0u; i < M; ++i)
+    {
+        for (auto r = i + 1; r < M; ++r)
+        {
+            if (m(r, i) == 0 || m(i, i) == 0)
+            {
+                return false;
+            }
+
+            const auto f = m(r, i) / m(i, i);
+            m(r, i) = 0;
+            for (auto j = i + 1; j < N; ++j)
+            {
+                m(r, j) -= m(i, j) * f;
+            }
+        }
+    }
+
+    original = m;
+    return true;
+}
+
+template <typename T, uint32_t M, uint32_t N>
+void gaussianEliminationPivot(Matrix<T, M, N>& m)
+{
+    // along diagonal
+    for (auto i = 0u; i < M; ++i)
+    {
+        pivotRows(m, i);
+
+        for (auto r = i + 1; r < M; ++r)
+        {
+            if (m(r, i) == 0 || m(i, i) == 0)
+            {
+                continue;
+            }
+
+            const auto f = m(r, i) / m(i, i);
+            m(r, i) = 0;
+            for (auto j = i + 1; j < N; ++j)
+            {
+                m(r, j) -= m(i, j) * f;
+            }
+        }
+    }
+}
+
+template <typename T, uint32_t M, uint32_t N>
+Matrix<T, M - 1, N - 1> eleminateRowColumn(const Matrix<T, M, N>& m, uint32_t r, uint32_t c)
+{
+    Matrix<T, M - 1, N - 1> em;
+
+    uint32_t a = 0;
+    for (auto i = 0u; i < M; ++i)
+    {
+        if (i == r)
+        {
+            continue;
+        }
+
+        uint32_t b = 0;
+        for (auto j = 0u; j < N; ++j)
+        {
+            if (j == c)
+            {
+                continue;
+            }
+            em(a, b) = m(i, j);
+            ++b;
+        }
+        ++a;
+    }
+
+    return em;
+}
+
+template <typename T>
+T det(const Matrix<T, 1, 1>& m)
+{
+    return m(0, 0);
+}
+
+template <typename T>
+T det(const Matrix<T, 2, 2>& m)
+{
+    const auto a = m(0, 0) * m(1, 1);
+    const auto b = m(0, 1) * m(1, 0);
+    if (std::nextafter(a, b) == b || std::nextafter(b, a) == a)
+    {
+        return 0;
+    }
+
+    return a - b;
+}
+
+// determinant using Gaussian elemination
+template <typename T, uint32_t M>
+T det(Matrix<T, M, M> m)
+{
+    gaussianEliminationPivot(m);
+    T a = m(0, 0);
+    for (auto i = 1u; i < M; ++i)
+    {
+        a *= m(i, i);
+    }
+
+    return a;
+}
+
+template <typename T>
+T det2(const Matrix<T, 1, 1>& m)
+{
+    return det(m);
+}
+
+template <typename T>
+T det2(const Matrix<T, 2, 2>& m)
+{
+    return det(m);
+}
+
+// slower but more accurate since there is no Gaussian elimination
+// that may introduce calculation errors
+template <typename T, uint32_t M>
+T det2(const Matrix<T, M, M>& m)
+{
+    T d = 0;
+    T sgn = 1; // start at (0,0) is (-1)^(1+1)
+    for (auto i = 0u; i < M; ++i)
+    {
+        if (m(0, i) != 0)
+        {
+            const auto subDeterminant = det2(eleminateRowColumn(m, 0, i));
+            const auto p = sgn * m(0, i) * subDeterminant;
+
+            if (std::signbit(p) != std::signbit(d) && (std::nextafter(d, -p) == p || std::nextafter(-p, d) == d))
+            {
+                d = 0;
+            }
+            else
+            {
+                d += p;
+            }
+        }
+        sgn = sgn > 0 ? -1. : 1.;
+    }
+    return d;
+}
+
+template <typename T, uint32_t M, uint32_t N, uint32_t R>
+Matrix<T, R, R> principalSubMatrix(const Matrix<T, M, N>& m)
+{
+    static_assert(R < M && R < N);
+    Matrix<T, R, R> psm;
+    for (auto i = 0u; i < R; ++i)
+    {
+        for (auto j = 0u; j < R; ++j)
+        {
+            psm(i, j) = m(i, j);
+        }
+    }
+    return psm;
+}
+
+// M is positive definite if determinants of each principal sub matrix
+// are positive. This is true if diagonal elements are all positive after gaussaian elimination
+template <typename T, uint32_t M>
+bool isPositiveDefinite(const Matrix<T, M, M>& m)
+{
+    if (!isSymmetric(m))
+    {
+        return false;
+    }
+    auto b(m);
+    gaussianElimination(b);
+    for (auto i = 0u; i < M; ++i)
+    {
+        if (b(i, i) <= 0)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+// check is positive semi definite by Gaussian elimination
+template <typename T, uint32_t M>
+bool isPositiveSemiDefinite(const Matrix<T, M, M>& m)
+{
+    if (!isSymmetric(m))
+    {
+        return false;
+    }
+    auto b(m);
+    gaussianElimination(b);
+    for (auto i = 0u; i < M; ++i)
+    {
+        if (b(i, i) < 0)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+// check if positive definite by calculating all principal sub matrix determinants
+template <typename T, uint32_t M>
+bool isPositiveDefinite2(const Matrix<T, M, M>& m)
+{
+    return isPositiveDefinite2(principalSubMatrix<T, M, M, M - 1>(m)) && det2(m) > 0;
+}
+
+template <typename T>
+bool isPositiveDefinite2(const Matrix<T, 2, 2>& m)
+{
+    return m(0, 0) > 0 && det(m) > 0;
+}
+
+template <typename T>
+bool isPositiveDefinite2(const Matrix<T, 1, 1>& m)
+{
+    return m(0, 0) > 0;
+}
+
+// check if positive semi definite by calculating all principal sub matrix determinants
+template <typename T, uint32_t M>
+bool isPositiveSemiDefinite2(const Matrix<T, M, M>& m)
+{
+    return isPositiveSemiDefinite2(principalSubMatrix<T, M, M, M - 1>(m)) && det2(m) >= 0;
+}
+
+template <typename T>
+bool isPositiveSemiDefinite2(const Matrix<T, 2, 2>& m)
+{
+    return m(0, 0) >= 0 && det(m) >= 0;
+}
+
+template <typename T>
+bool isPositiveSemiDefinite2(const Matrix<T, 1, 1>& m)
+{
+    return m(0, 0) >= 0;
+}
+
+template <typename T, uint32_t M, uint32_t N>
+void randomizePositive(Matrix<T, M, N>& m, T maxValue)
+{
+    for (auto i = 0u; i < M; ++i)
+    {
+        for (auto j = 0u; j < N; ++j)
+        {
+            m(i, j) = maxValue * (rand() % 10000) / 10000.0;
+        }
+    }
+}
+
+template <typename T, uint32_t M, uint32_t N>
+void randomize(Matrix<T, M, N>& m, T valueRange)
+{
+    for (auto i = 0u; i < M; ++i)
+    {
+        for (auto j = 0u; j < N; ++j)
+        {
+            m(i, j) = valueRange * (rand() % 10000) / 10000.0 - valueRange / 2;
+        }
+    }
+}
+
+template <typename T, uint32_t M>
+bool isLowerTriangular(const Matrix<T, M, M>& m)
+{
+    for (uint32_t i = 0; i < M; ++i)
+    {
+        for (uint32_t j = i + 1; j < M; ++j)
+        {
+            if (m(i, j) != 0)
+            {
+                return false;
+            }
+        }
+    }
+    return true;
+}
 } // namespace math
--- test/bwe/MatrixTests.cpp
+++ test/bwe/MatrixTests.cpp
@@ -30,8 +30,15 @@ TEST(MatrixTest, mul)
 TEST(MatrixTest, cholesky)
 {
     math::Matrix<double, 3> v({5.6, -156000.0, 19.9});
-    auto m = v * math::transpose(v);
+    const auto m = math::outerProduct(v);
 
+    EXPECT_EQ(det2(math::principalSubMatrix<double, 3, 3, 2>(m)), 0.0);
+    EXPECT_EQ(det(math::principalSubMatrix<double, 3, 3, 2>(m)), 0.0);
+    EXPECT_EQ(det(m), 0.0);
+    EXPECT_EQ(det2(m), 0.0);
+
+    EXPECT_TRUE(math::isPositiveSemiDefinite2(m));
+    EXPECT_TRUE(math::isPositiveSemiDefinite(m));
     auto L = choleskyDecompositionLL(m);
     EXPECT_EQ(L.getColumn(0), v);
     auto c = L * transpose(L);
@@ -46,9 +53,154 @@ TEST(MatrixTest, choleskyStability)
         {5.5034655381268649E-19, 102615.51002615986, -3.4396659613292905E-20},
         {-3.9095946203353344E-29, -3.4396659613292905E-20, 2.443496637709584E-30}});
 
+    ASSERT_TRUE(math::isSymmetric(m));
+    EXPECT_TRUE(isPositiveSemiDefinite2(m));
+    auto mine = std::abs(math::min(m));
+    auto maxe = std::abs(math::max(m));
+
+    auto mf = 0.001000;
+    double detti = math::det(m);
+    auto d2 = math::det2(m);
+    ASSERT_GE(math::det(m), 0);
     auto L = choleskyDecompositionLL(m);
     auto c = L * transpose(L);
     EXPECT_FALSE(m == c);
+    auto d = m - c;
+    auto nrm = math::norm(m - c);
+    EXPECT_LT(math::norm(m - c), 1.E-10);
     EXPECT_TRUE(math::isValid(L));
     // should check determinant of m - c
+}
+
+#define EXPECT_ERROR_LT(a, b, relativeError)                                                                           \
+    {                                                                                                                  \
+        if (std::min(std::abs(a), std::abs(b)) == 0)                                                                   \
+        {                                                                                                              \
+            EXPECT_LT(std::max(std::abs(a), std::abs(b)), relativeError);                                              \
+        }                                                                                                              \
+        else                                                                                                           \
+        {                                                                                                              \
+            EXPECT_GT(std::min(std::abs(a), std::abs(b)) / std::max(std::abs(a), std::abs(b)), 1.0 - relativeError);   \
+        }                                                                                                              \
+    }
+
+TEST(MatrixTest, determinant)
+{
+    math::Matrix<double, 5, 5> m;
+
+    const auto acceptableRelativeError = 0.000000001;
+    for (auto k = 0u; k < 15000; ++k)
+    {
+        randomize(m, 10000.0);
+
+        const auto d1 = det2(m);
+        const auto d2 = det2(transpose(m));
+        const auto d3 = det(m);
+        const auto d4 = det(transpose(m));
+
+        if (d2 != d4)
+        {
+            if (det(m) == 0 || det(transpose(m)) == 0)
+            {
+            }
+            EXPECT_ERROR_LT(det(m), det(transpose(m)), acceptableRelativeError);
+        }
+        if (d3 != d2)
+        {
+            EXPECT_ERROR_LT(det(m), det2(transpose(m)), acceptableRelativeError);
+        }
+        if (d1 != d4)
+        {
+            EXPECT_ERROR_LT(det2(m), det(transpose(m)), acceptableRelativeError);
+        }
+        if (d1 != d2)
+        {
+            const auto a1 = det2(m);
+            const auto a2 = det2(transpose(m));
+            const auto a3 = det(m);
+            const auto a4 = det(transpose(m));
+            const auto nd1 = std::nextafter(d1, d2);
+            EXPECT_ERROR_LT(d1, d2, acceptableRelativeError);
+        }
+    }
+}
+
+TEST(MatrixTest, submatrix)
+{
+    math::Matrix<double, 5, 5> m;
+    math::randomize(m, 1000.0);
+
+    auto s2 = math::principalSubMatrix<double, m.rows(), m.columns(), 2>(m);
+    EXPECT_EQ(m(0, 0), s2(0, 0));
+    EXPECT_EQ(m(0, 1), s2(0, 1));
+    EXPECT_EQ(m(1, 0), s2(1, 0));
+    EXPECT_EQ(m(1, 1), s2(1, 1));
+}
+
+TEST(MatrixTest, posDefininte)
+{
+    math::Matrix<double, 3, 3> m2({{10.0, 5., 2.}, {5., 3., 2.}, {2., 2., 3.}});
+    ASSERT_TRUE(isSymmetric(m2));
+    EXPECT_TRUE(isPositiveDefinite(m2));
+}
+
+TEST(MatrixTest, cholesky3)
+{
+    math::Matrix<double, 3> v({10, 5, 3});
+    auto m = math::outerProduct(v);
+
+    ASSERT_TRUE(math::isSymmetric(m));
+    EXPECT_TRUE(math::isPositiveSemiDefinite(m));
+
+    auto L = choleskyDecompositionLL(m);
+    auto c = L * transpose(L);
+    EXPECT_EQ(m, c);
+}
+
+TEST(MatrixTest, choleskyOuterProduct)
+{
+    for (auto i = 0u; i < 10000; ++i)
+    {
+        math::Matrix<double, 3> v({10, 5, 3});
+        math::randomize(v, 10000.0);
+        auto m = math::outerProduct(v);
+
+        const auto pd = isPositiveDefinite2(m);
+        const auto psd = isPositiveSemiDefinite2(m);
+        EXPECT_TRUE(psd);
+        EXPECT_FALSE(pd);
+        const auto L = choleskyDecompositionLL(m);
+        EXPECT_TRUE(math::isLowerTriangular(L));
+        EXPECT_EQ(m, L * transpose(L));
+    }
+}
+
+TEST(MatrixTest, choleskyOnSum)
+{
+    for (int i = 0; i < 500; ++i)
+    {
+        math::Matrix<double, 3> v({10, 5, 3});
+        math::randomize(v, 10000.0);
+        auto m = math::outerProduct(v);
+
+        auto sum = m;
+        for (int j = 0; j < 100; ++j)
+        {
+            math::Matrix<double, 3> w;
+            math::randomize(w, 10.0);
+            auto smallDiff = math::outerProduct(w);
+            sum += smallDiff;
+        }
+
+        EXPECT_TRUE(math::isPositiveSemiDefinite2(sum));
+        auto L = math::choleskyDecompositionLL(sum);
+        EXPECT_TRUE(math::isLowerTriangular(L));
+        EXPECT_TRUE(math::isSymmetric(sum));
+        if (sum != L * transpose(L))
+        {
+            const auto d = sum - L * transpose(L);
+            const auto nrm = math::norm(d);
+            EXPECT_LT(nrm, 1.E-8);
+        }
+    }
 }
