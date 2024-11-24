7950db716afafac5faa1ed2ab88dd9250de8d038
Olof Källander <whistler.boardrider@gmail.com>
2024-11-24 16:51:08 +0100

    fix simple client

--- examples/simpleclient/src/main.ts
+++ examples/simpleclient/src/main.ts
@@ -1,6 +1,5 @@
-import { addSimulcastSdpLocalDescription } from './simulcast';
+import {addSimulcastSdpLocalDescription} from './simulcast';
 
-const joinButton: HTMLButtonElement = <HTMLButtonElement>document.getElementById('join');
 const endpointIdLabel: HTMLLabelElement = <HTMLLabelElement>document.getElementById('endpointId');
 const dominantSpeakerLabel: HTMLLabelElement = <HTMLLabelElement>document.getElementById('dominantSpeaker');
 const audioElementsDiv: HTMLDivElement = <HTMLDivElement>document.getElementById('audioElements');
@@ -13,23 +12,31 @@ let remoteMediaStreams: Set<string> = new Set();
 
 const serverUrl = 'https://localhost:8081/conferences/';
 
+interface UserVideoMapItem
+{
+    ssrc: number;
+    msid: String;
+    element: HTMLVideoElement;
+}
+
+let receivers = new Map<string, UserVideoMapItem>();
+
 // Keeps a long-poll running for simpleserver to simpleclient communication
-async function startPoll() {
+async function startPoll()
+{
     const url = serverUrl + 'endpoints/' + endpointId + '/poll';
-    const requestInit: RequestInit = {
-        method: 'GET',
-        mode: 'cors',
-        cache: 'no-store'
-    };
+    const requestInit: RequestInit = {method : 'GET', mode : 'cors', cache : 'no-store'};
 
     const request = new Request(url, requestInit);
     const result = await fetch(request);
-    if (result.status === 200) {
+    if (result.status === 200)
+    {
         const resultJson = await result.json();
         console.log('Poll result ' + JSON.stringify(resultJson));
         onPollMessage(resultJson);
-
-    } else if (result.status !== 204) {
+    }
+    else if (result.status !== 204)
+    {
         console.error('Poll failed ' + result.statusText)
         return;
     }
@@ -37,12 +44,11 @@ async function startPoll() {
     startPoll();
 }
 
-async function onPollMessage(resultJson: any) {
-    if (resultJson.type === 'offer') {
-        const remoteDescription = new RTCSessionDescription({
-            type: 'offer', 
-            sdp: resultJson.payload
-        });
+async function onPollMessage(resultJson: any)
+{
+    if (resultJson.type === 'offer')
+    {
+        const remoteDescription = new RTCSessionDescription({type : 'offer', sdp : resultJson.payload});
         console.log("Got offer " + JSON.stringify(remoteDescription));
         await peerConnection.setRemoteDescription(remoteDescription);
 
@@ -50,36 +56,31 @@ async function onPollMessage(resultJson: any) {
 
         // Add simulcast layers to the video stream with SDP munging
         const mediaTracks = localMediaStream.getVideoTracks();
-        mediaTracks.forEach(mediaTrack => {
-            localDescription = addSimulcastSdpLocalDescription(localDescription, mediaTrack.id, 3);
-        });
+        mediaTracks.forEach(
+            mediaTrack => { localDescription = addSimulcastSdpLocalDescription(localDescription, mediaTrack.id, 3); });
 
         await peerConnection.setLocalDescription(localDescription);
     }
 }
 
-// SMB does not support trickle-ice, therefore we wait until the gathering is complete to send the SDP 
+// SMB does not support trickle-ice, therefore we wait until the gathering is complete to send the SDP
 // answer so that it includes all candidates
-async function onIceGatheringStateChange(event: Event) {
+async function onIceGatheringStateChange(event: Event)
+{
     const iceGatheringState = peerConnection.iceGatheringState;
 
-    if (iceGatheringState === 'complete') {
+    if (iceGatheringState === 'complete')
+    {
         console.log('ICE candidate gathering complete');
-        if (peerConnection.localDescription) {
+        if (peerConnection.localDescription)
+        {
             console.log('Sending answer ' + JSON.stringify(peerConnection.localDescription));
 
             const url = serverUrl + 'endpoints/' + endpointId + '/actions';
-            const body = {
-                type: 'answer',
-                payload: peerConnection.localDescription.sdp
-            };
+            const body = {type : 'answer', payload : peerConnection.localDescription.sdp};
 
-            const requestInit: RequestInit = {
-                method: 'POST',
-                mode: 'cors',
-                cache: 'no-store',
-                body: JSON.stringify(body)
-            };
+            const requestInit:
+                RequestInit = {method : 'POST', mode : 'cors', cache : 'no-store', body : JSON.stringify(body)};
             const request = new Request(url, requestInit);
             const result = await fetch(request);
 
@@ -88,16 +89,21 @@ async function onIceGatheringStateChange(event: Event) {
     }
 }
 
-function onTrack(event: RTCTrackEvent) {
-    for (const stream of event.streams) {
-        if (stream.id === 'smbvideomslabel' || stream.id === 'smbaudiomslabel' || remoteMediaStreams.has(stream.id)) {
+function onTrack(event: RTCTrackEvent)
+{
+    for (const stream of event.streams)
+    {
+        if (remoteMediaStreams.has(stream.id))
+        {
             continue;
         }
 
         remoteMediaStreams.add(stream.id);
-        console.log(`Added remote stream ${stream.id} audio ${stream.getAudioTracks().length} video ${stream.getVideoTracks().length}`);
+        console.log(`Added remote stream ${stream.id} audio ${stream.getAudioTracks().length} video ${
+            stream.getVideoTracks().length}`);
 
-        if (stream.getAudioTracks().length !== 0) {
+        if (stream.getAudioTracks().length !== 0)
+        {
             const audioElement = new Audio();
             audioElement.autoplay = true;
             audioElement.srcObject = stream;
@@ -105,68 +111,112 @@ function onTrack(event: RTCTrackEvent) {
             console.log('Added audio element ' + stream.id);
         }
 
-        if (stream.getVideoTracks().length !== 0) {
+        if (stream.getVideoTracks().length !== 0)
+        {
             const videoElement = <HTMLVideoElement>document.createElement('video');
             videoElement.autoplay = true;
             videoElement.srcObject = stream;
             videoElement.loop = true;
-            videoElement.width = 640;
-            videoElement.height = 480;
+            videoElement.width = 320;
+            videoElement.height = 180;
+            videoElement.muted = false;
             videoElementsDiv.appendChild(videoElement);
+            receivers.set(event.track.id, {ssrc : 0, msid : event.track.id as String, element : videoElement});
             console.log('Added video element ' + stream.id);
         }
     }
 }
 
-function onDataChannelMessage(event: MessageEvent<any>) {
+function onDataChannelMessage(event: MessageEvent<any>)
+{
     console.log('onDataChannelMessage ' + event.data);
     const message = JSON.parse(event.data);
-    if (message.type === 'DominantSpeaker') {
+    if (message.type === 'DominantSpeaker')
+    {
         const dominantSpeakerEndpointId = message.endpoint;
 
         dominantSpeakerLabel.innerText = dominantSpeakerEndpointId;
 
-        if (dominantSpeakerEndpointId !== endpointId) {
-            const pinMessage = {
-                type: 'PinnedEndpointsChanged',
-                pinnedEndpoints: [
-                    dominantSpeakerEndpointId
-                ]
-            };
-    
+        if (dominantSpeakerEndpointId !== endpointId)
+        {
+            const pinMessage = {type : 'PinnedEndpointsChanged', pinnedEndpoints : [ dominantSpeakerEndpointId ]};
+
             localDataChannel.send(JSON.stringify(pinMessage));
             console.log('Sent data channel message ' + JSON.stringify(pinMessage));
         }
     }
+    else if (message.type === 'UserMediaMap')
+    {
+        for (const endpoint of message.endpoints)
+        {
+            for (var ssrc of endpoint.ssrcs)
+            {
+                console.log("ssrc {} speaking", ssrc)
+
+                var rtpReceivers = peerConnection.getReceivers();
+                for (var rtpReceiver of rtpReceivers)
+                {
+                    var ssrcs = rtpReceiver.getSynchronizationSources();
+                    if (ssrcs.length == 0 || rtpReceiver.track.kind != "video")
+                    {
+                        continue;
+                    }
+
+                    if (ssrcs[0].source == ssrc)
+                    {
+                        var mapItem = receivers.get(rtpReceiver.track.label);
+                        if (videoElementsDiv.firstChild != mapItem.element)
+                        {
+                            videoElementsDiv.removeChild(mapItem.element);
+                            videoElementsDiv.insertBefore(mapItem.element, videoElementsDiv.firstChild);
+                        }
+                        return;
+                    }
+                }
+            }
+
+            return;
+        }
+    }
 }
 
-async function joinClicked() {
-    console.log('Join clicked');
+async function joinClicked()
+{
+    console.log('Join clicked ...');
+
+    (async () => {
+        await navigator.mediaDevices.getUserMedia({audio : true, video : true});
+        let devices = await navigator.mediaDevices.enumerateDevices();
+        console.log(devices);
+    })();
 
     peerConnection = new RTCPeerConnection();
     peerConnection.onicegatheringstatechange = onIceGatheringStateChange;
     peerConnection.ontrack = onTrack;
     console.log('Created RTCPeerConnection');
 
-    localMediaStream = await navigator.mediaDevices.getUserMedia({
-        audio: true,
-        video: true
-    });
+    const videoDeviceId = sessionStorage.getItem('selectedVideoDevice');
+    const audioDeviceId = sessionStorage.getItem('selectedAudioDevice');
+
+    const contraints = {video : {deviceId : videoDeviceId}, audio : {deviceId : audioDeviceId}};
+
+    localMediaStream = await navigator.mediaDevices.getUserMedia(contraints);
+    const localVideo = document.getElementById("localVideo") as HTMLVideoElement;
+    localVideo.srcObject = localMediaStream;
+    localVideo.loop = true;
+    localVideo.width = 160;
+    localVideo.height = 90;
+
     console.log('Got local stream ' + JSON.stringify(localMediaStream));
     localMediaStream.getTracks().forEach(track => peerConnection.addTrack(track, localMediaStream));
 
-    localDataChannel = peerConnection.createDataChannel("webrtc-datachannel", { ordered: true });
+    localDataChannel = peerConnection.createDataChannel("webrtc-datachannel", {ordered : true});
     localDataChannel.onmessage = onDataChannelMessage;
-        
+
     const url = serverUrl + 'endpoints/';
     const body = {};
 
-    const requestInit: RequestInit = {
-        method: 'POST',
-        mode: 'cors',
-        cache: 'no-store',
-        body: JSON.stringify(body)
-    };
+    const requestInit: RequestInit = {method : 'POST', mode : 'cors', cache : 'no-store', body : JSON.stringify(body)};
     const request = new Request(url, requestInit);
     const result = await fetch(request);
     const resultJson = await result.json();
@@ -178,8 +228,72 @@ async function joinClicked() {
     startPoll()
 }
 
-function main() {
+async function listAudioDevices()
+{
+    try
+    {
+        const devices = await navigator.mediaDevices.enumerateDevices();
+        const audioDevices = devices.filter(device => device.kind === 'audioinput');
+        const selectElement = document.getElementById('audioDevices') as HTMLSelectElement;
+        selectElement.innerHTML = '';
+        audioDevices.forEach(device => {
+            const option = document.createElement('option');
+            option.value = device.deviceId;
+            option.text = device.label || `Device ${device.deviceId}`;
+            selectElement.appendChild(option);
+        });
+
+        const savedDeviceId = sessionStorage.getItem('selectedAudioDevice');
+        if (savedDeviceId)
+        {
+            selectElement.value = savedDeviceId;
+        }
+
+        selectElement.addEventListener('change',
+            () => { sessionStorage.setItem('selectedAudioDevice', selectElement.value); });
+    }
+    catch (error)
+    {
+        console.error("Error accessing audio devices: ", error);
+    }
+}
+
+async function listVideoDevices()
+{
+    try
+    {
+        const devices = await navigator.mediaDevices.enumerateDevices();
+        const videoDevices = devices.filter(device => device.kind === 'videoinput');
+        const selectElement = document.getElementById('videoDevices') as HTMLSelectElement;
+        selectElement.innerHTML = '';
+        videoDevices.forEach(device => {
+            const option = document.createElement('option');
+            option.value = device.deviceId;
+            option.text = device.label || `Device ${device.deviceId}`;
+            selectElement.appendChild(option);
+        });
+        const savedDeviceId = sessionStorage.getItem('selectedVideoDevice');
+        if (savedDeviceId)
+        {
+            selectElement.value = savedDeviceId;
+        }
+
+        selectElement.addEventListener('change',
+            async () => { sessionStorage.setItem('selectedVideoDevice', selectElement.value); });
+    }
+    catch (error)
+    {
+        console.error("Error accessing video devices: ", error);
+    }
+}
+
+async function main()
+{
+    var joinButton = document.getElementById('join') as HTMLButtonElement;
     joinButton.onclick = joinClicked;
+
+    await listAudioDevices();
+    await listVideoDevices();
 }
 
 main();
